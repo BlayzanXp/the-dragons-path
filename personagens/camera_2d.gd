8856316d94b2280922348164cf86bc7eb3150e06
Blayzan <166439791+BlayzanXp@users.noreply.github.com>
2025-05-05 19:54:29 -0300

    1.0

--- personagens/camera_2d.gd
+++ personagens/camera_2d.gd
@@ -1,24 +1,11 @@
-extends Camera2D
-
-@onready var player = get_node("/root/Node2D/Node2D/CharacterBody2D/Player")  # Caminho absoluto
-
-# Definindo o tamanho do mapa
-var mapa_largura = 1920  # Largura do mapa
-var mapa_altura = 1080  # Altura do mapa
+@onready var camera: Camera2D = $Camera2D
+@onready var tilemap: TileMap = $"../TileMap"
 
 func _ready():
-	# Certifique-se de que a câmera está configurada para seguir o jogador.
-	offset = Vector2(0, 0)
-
-func _process(_delta):  # Agora o delta é ignorado
-	# Acompanhar o jogador
-	var nova_pos = player.position
+	var map_rect = tilemap.get_used_rect()
+	var cell_size = tilemap.cell_quadrant_size  # Ou tilemap.tile_set.tile_size se estiver na Godot 4.0+
 	
-	# Limitar a posição da câmera no eixo X (horizontal)
-	nova_pos.x = clamp(nova_pos.x, mapa_largura / 2.0, mapa_largura - mapa_largura / 2.0)
-	
-	# Limitar a posição da câmera no eixo Y (vertical)
-	nova_pos.y = clamp(nova_pos.y, mapa_altura / 2.0, mapa_altura - mapa_altura / 2.0)
-	
-	# Aplicar a posição calculada à câmera
-	position = nova_pos
+	camera.limit_left = map_rect.position.x * cell_size
+	camera.limit_top = map_rect.position.y * cell_size
+	camera.limit_right = (map_rect.position.x + map_rect.size.x) * cell_size
+	camera.limit_bottom = (map_rect.position.y + map_rect.size.y) * cell_size
